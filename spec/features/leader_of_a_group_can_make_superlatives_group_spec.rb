7cafc8132ffe9ff87418fbfd228650946f4c2794
Paul Dornfeld <pdornfel@gmail.com>
2014-01-31 12:52:54 -0500

    added two feature stories for testing

---
+++ spec/features/leader_of_a_group_can_make_superlatives_group_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature "A leader of a group can create superlatives for a group" do
+
+  scenario "A leader wants to sign up"
+
+  scenario "A leader wants to invite people via email"
+
+  scenario "A leader wants to add people to be included in superlatives"
+
+  scenario "Only the admin can see pages pertaining to admin panel"
+
+end
---
+++ spec/features/users_can_sign_up_with_twitter_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+feature "A user who has been invited by the admin can sign up with twitter" do
+
+  scenario "As an invited user I can sign up with twitter"
+
+end
